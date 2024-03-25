b90f5fd5bfa3da82f3f9dfa9740a309f701243b4
rohanclement <rclement5700@gmail.com>
2024-03-25 16:01:06 -0700

    Replace LinearLayout with GridLayout

--- app/src/main/java/com/clementcorporation/empowerexample/ui/main/MainFragment.kt
+++ app/src/main/java/com/clementcorporation/empowerexample/ui/main/MainFragment.kt
@@ -1,13 +1,17 @@
 package com.clementcorporation.empowerexample.ui.main
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.clementcorporation.empowerexample.databinding.FragmentMainBinding
 
+data class EmpowerObject(val color: Int, val title: String)
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
@@ -16,39 +20,55 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater)
-        renderTreeChart(listOf(3,2,4,1), binding.mainLayout)
+        val weights = listOf(3,2,4,1)
+        renderTreeChart(weights, binding.mainLayout)
         return binding.root
     }
 
-    private fun renderTreeChart(list: List<Int>, parent: ViewGroup) {
-        val colors = listOf(
-            requireContext().getColor(android.R.color.holo_red_dark),
-            requireContext().getColor(android.R.color.holo_blue_dark),
-            requireContext().getColor(android.R.color.holo_green_dark),
-            requireContext().getColor(android.R.color.black),
-            requireContext().getColor(android.R.color.holo_purple),
-            requireContext().getColor(android.R.color.holo_orange_dark)
+    private fun renderTreeChart(weights: List<Int>, parent: ViewGroup) {
+        val tools = listOf(
+            EmpowerObject(requireContext().getColor(android.R.color.holo_red_dark), "Stocks"),
+            EmpowerObject(requireContext().getColor(android.R.color.holo_blue_dark), "Bonds"),
+            EmpowerObject(requireContext().getColor(android.R.color.holo_purple), "Crypto"),
+            EmpowerObject(requireContext().getColor(android.R.color.holo_orange_dark), "Cash")
+            //ADD COLOR PER WEIGHT
         )
-        parent.viewTreeObserver.addOnGlobalLayoutListener {
-            var height = parent.height
-            var width = parent.width
-            var divideVertically = true
-            var colorsIndex = 0
-            val subView = RelativeLayout(requireContext()).apply {
-                setBackgroundColor(colors[colorsIndex])
-                layoutParams = RelativeLayout.LayoutParams(width, height)
-            }
-            parent.addView(subView)
-            list.forEach { _ ->
-                if (divideVertically) width /= 2
-                else height /= 2
-                divideVertically = !divideVertically
-                if (colorsIndex > colors.size - 1) colorsIndex = 0 else colorsIndex++
-                subView.addView(RelativeLayout(requireContext()).apply {
-                    setBackgroundColor(colors[colorsIndex])
-                    layoutParams = RelativeLayout.LayoutParams(width, height)
-                })
-            }
+        val grid = GridLayout(requireContext()).apply {
+            rowCount = weights.size/2
+            columnCount = weights.size/2
+            orientation = GridLayout.VERTICAL
+            alignmentMode = GridLayout.ALIGN_MARGINS
+            isColumnOrderPreserved = false
+            isRowOrderPreserved = false
+            useDefaultMargins = false
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+            )
         }
+        weights.forEachIndexed { index, weight ->
+            val tool = tools[index]
+            val text = TextView(requireContext()).apply {
+                text = tool.title
+                textSize = 14f
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+                gravity = Gravity.CENTER
+                layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+                setTextColor(resources.getColor(android.R.color.white, requireContext().theme))
+            }
+            val newView = LinearLayout(requireContext()).apply {
+                addView(text)
+                setBackgroundColor(tool.color)
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                )
+            }
+            val rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, weight.toFloat())
+            val colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, weight.toFloat())
+            val params = GridLayout.LayoutParams(rowSpan, colSpan)
+            grid.addView(newView, params)
+        }
+        parent.addView(grid)
     }
 }
